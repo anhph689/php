8d4554f18a526d86b5d3785e5759df4223e6d8a4
anhph689 <anhph689@gmail.com>
2025-07-24 10:58:29 +0700

    lab 1

---
+++ Lab1/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Ví dụ về xuất chuỗi, biến
+ * 1. Xuất chuỗi
+ */
+echo "Hello <br>";
+echo ("Hello <br>");
+echo "Hello World <br>";
+echo "Hello", " World <br>";
+
+/**
+ * 2. Xuất biến
+ */
+$str = 'Hello World';
+$x = 100;
+$y = 1.23;
+
+echo "Chuỗi là : $str <br>";
+echo "Số nguyên là : $x <br>";
+echo "Số thập phân là : $y <br>";
+?>

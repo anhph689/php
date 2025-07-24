d1221e52020c32e6d5aad7c8bd2a25dbaf5a7fff
anhph689 <anhph689@gmail.com>
2025-07-24 09:33:46 +0700

    bai 1

---
+++ Bai_1/index.php
@@ -0,0 +1,47 @@
+<?php
+//1. Lệnh chú thích (Comment)
+//Không biên dịch được trên server
+//Gõ 2 dấu // hoặc /** */ ở đầu hoặc sử dung tổ hợp phím Ctrl + /
+
+//Ghi chú thích tại đây
+
+/**
+ * Hoặc chú thích tại đây
+ * 
+ */
+
+//Chú thích (Commment)
+//echo "Hello world";
+
+//2. Chuỗi giá trị
+//Echo và print
+
+//Đối với print
+//print true;
+
+//3. Khai báo biến
+// $message = "Hello World";
+// echo $message;
+
+//4. Các kiểu dữ liệu
+//Kiểu số nguyên(Interger - Int)
+// $int = 1000;
+// echo $int;
+
+//Kiểu số thập phân(Float)
+// $float = 1.6868;
+// echo $float;
+
+//Kiểu dữ liệu Boolean
+// $boolean = true; //hoặc false
+// echo $boolean;
+
+//Kiểu dữ liệu null
+// $null = null;
+// echo $null;
+
+//Kiểu dữ liệu chuỗi(String)
+$string = "Hello ";
+$b = "World";
+echo $string .= $b;
+?>
